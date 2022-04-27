e0d13e803b05487a7c8571fbc353a6bcd65abcab
Cristian Perez <cristian.perez@gocity.com>
2022-04-27 12:29:54 +0100

    first commit

---
+++ src/main/java/com/gocity/kafkaproducer/service/KafkaProducer.java
@@ -0,0 +1,28 @@
+package com.gocity.kafkaproducer.service;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final NewTopic topic;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, NewTopic topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+        sendBatchOfEvents();
+    }
+
+    public void sendEvent(String key, String value) {
+        kafkaTemplate.send(topic.name(), key, value);
+    }
+
+    public void sendBatchOfEvents() {
+        for(int i = 0; i < 100; i++) {
+            sendEvent("k", "v".concat(String.valueOf(i)));
+        }
+    }
+}
